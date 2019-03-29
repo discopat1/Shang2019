a5e5eff03ee9edcf039c71644e657f6fde00f0ae
AshleyH <ladynirvana@hotmail.com>
2019-03-29 15:19:26 -0600

    more server changes to try to get heroku going

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-require("dotenv").config()
-const path = require("path");
+const path = require ("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
@@ -8,15 +7,24 @@ const app = express();
 const passport = require("passport");
 const users = require("./routes/API/users");
 
-//PORT
-const PORT = process.env.PORT || 5000;
 
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to the Mongo DB
+mongoose
+.connect(process.env.MONGODB_URI || "mongodb://localhost/shangrila")
+.then(console.log("Connected to Mongodb"))
+.catch(err=> console.log(err));
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -25,33 +33,22 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// Add routes, both API and view
+app.use(routes);
+app.use('/API/users', users);
+
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(_dirname, 'client','build', 'index.html' ));
+    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html' ));
   });
 }
 
-// Add routes, both API and view
-app.use(routes);
-app.use(users)
-
-
-// DB Config
-// const db = require("./config/keys").mongoURI;
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shangrila",{ useNewUrlParser:true})
-.then(console.log("Connected to Mongodb"));
-
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-
-
-
-
+//PORT
+const PORT = process.env.PORT || 5000;
 
 
 // Start the API server
