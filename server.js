f195e16c6bee6e849806033af336f8f291cc679c
AshleyH <ladynirvana@hotmail.com>
2019-03-29 21:13:38 -0600

    removed service worker all functioning locally, herkou build failing still

--- server.js
+++ server.js
@@ -9,24 +9,10 @@ const users = require("./routes/API/users");
 //PORT
 const PORT = process.env.PORT || 5000;
 
-// DB Config
-// const db = require("./config/keys").mongoURI;
-
-// Connect to the Mongo DB
-mongoose
-.connect(process.env.MONGODB_URI || "mongodb://localhost/shangrila")
-.then(console.log("Connected to Mongodb"))
-.catch(err=> console.log(err));
-
-
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -35,21 +21,25 @@ app.use(
 );
 app.use(bodyParser.json());
 
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  
 }
-
 // Add routes, both API and view
 app.use(routes);
+app.use(users)
 
 
+// DB Config
+// const db = require("./config/keys").mongoURI;
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shangrila",{ useNewUrlParser:true})
+.then(console.log("Connected to Mongodb"));
 
-
-
-
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
 
 // Start the API server
 app.listen(PORT, function() {
