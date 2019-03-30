8bcc8b748450eafab4c874afe3477568fb0e7443
AshleyH <ladynirvana@hotmail.com>
2019-03-29 18:08:49 -0600

    capitalization

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 
 module.exports = {
-    mongoURI: "mongodb://heroku_1k9922gz:Sterling1@ds117846.mlab.com:17846/heroku_1k9922gz",
+    mongoURI: " mongodb://heroku_1k9922gz:4nqdivk6q3152e338aaiquf8op@ds117846.mlab.com:17846/heroku_1k9922gz",
     secretOrKey: "secret"
     
   };
+
+ 
