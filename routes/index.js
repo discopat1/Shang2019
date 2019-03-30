881f063d0cb5959b30023317ef334d436ba0219d
AshleyH <ladynirvana@hotmail.com>
2019-03-29 21:33:37 -0600

    tried changing up the router.use inside index.js of backend routes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const apiRoutes = require("./API");
 
 // API Routes
 router.use("/api", apiRoutes);
-
+const root = require('path').join(__dirname, 'client', 'build')
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile('index.html', { root });
 });
 
 module.exports = router;
+
+
+
+    
