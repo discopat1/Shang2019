f4f1f63fc054a8235915ffdc889ce1cb49d2f992
AshleyH <ladynirvana@hotmail.com>
2019-03-29 22:50:54 -0600

    making sure stage works

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./API");
 
 // API Routes
 router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
 
 
 module.exports = router;
